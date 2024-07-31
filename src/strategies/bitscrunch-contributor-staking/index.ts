c7417622ad6096803f4c34e6c982697be304b3f2
Raghav-bitsCrunch <rajaragavan@bitscrunch.com>
2024-07-31 17:51:29 +0530

    Added: bitCrunch Contributor Staking

---
+++ src/strategies/bitscrunch-contributor-staking/index.ts
@@ -0,0 +1,39 @@
+import { BigNumberish } from '@ethersproject/bignumber';
+import { getAddress } from '@ethersproject/address';
+import { Multicaller } from '../../utils';
+
+export const author = 'bonustrack';
+export const version = '0.1.1';
+
+const abi = [
+  'function isStaked(address _contributor) external view returns(bool)'
+];
+
+export async function strategy(
+  space,
+  network,
+  provider,
+  addresses,
+  options,
+  snapshot
+): Promise<Record<string, number>> {
+  const blockTag = typeof snapshot === 'number' ? snapshot : 'latest';
+
+  const multi = new Multicaller(network, provider, abi, { blockTag });
+  addresses.forEach((address) =>
+    multi.call(address, options.address, 'isStaked', [address])
+  );
+  const result: Record<string, BigNumberish> = await multi.execute();
+
+  return Object.fromEntries(
+    Object.entries(result).map(([address, staked]) => {
+      // console.log({ staked });
+      if (staked) {
+        return [getAddress(address), 1];
+      } else {
+        // console.log('hello');
+        return [getAddress(address), 0];
+      }
+    })
+  );
+}
